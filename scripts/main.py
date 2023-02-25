ad4c13f4b977a423802675f0c7dff70992cfa3c8
Ftps <63702646+Tps-F@users.noreply.github.com>
2023-02-25 20:57:41 +0900

    refactoring & add status

--- scripts/main.py
+++ scripts/main.py
@@ -1,26 +1,29 @@
-import os
-import numpy as np
-import cv2
 import json
-from lavis.models import load_model_and_preprocess
-import torch
+import os
+
+import cv2
 import gradio as gr
-import modules.ui
-import modules.scripts as scripts
-from modules import script_callbacks
+import numpy as np
+import torch
+from lavis.models import load_model_and_preprocess
 from PIL import Image
+
+from modules import script_callbacks
+import modules.scripts as scripts
+import modules.ui
+
 
 model_namelist, model_typelist, model_history = [], [], ""
 model, vis_processors = "", {}
 
 
 def get_device():
-    if (torch.cuda.is_available()):
-        return 'cuda'
-    elif (torch.backends.mps.is_available()):
-        return 'mps'
+    if torch.cuda.is_available():
+        return "cuda"
+    elif torch.backends.mps.is_available():
+        return "mps"
     else:
-        return 'cpu'
+        return "cpu"
 
 
 def load_data(file_name: str = f"{scripts.basedir()}/model.json") -> dict:
@@ -44,7 +47,10 @@ def get_model_type() -> list:
 def load_model(model_type):
     global model_history
     data = load_data()
-    name, modeltype = data["model"][model_type]["name"], data["model"][model_type]["type"]
+    name, modeltype = (
+        data["model"][model_type]["name"],
+        data["model"][model_type]["type"],
+    )
     if modeltype != model_history:
         global model, vis_processors
 
@@ -60,13 +66,22 @@ def load_model(model_type):
 
 
 def gen_caption(image, caption_type, length_penalty, repetition_penalty, temperature):
+    print("generating...")
     device = get_device()
     image = vis_processors["eval"](image).unsqueeze(0).to(device)
     if caption_type == "Beam Search":
-        caption = model.generate({"image": image},length_penalty=length_penalty, repetition_penalty=repetition_penalty)
+        caption = model.generate(
+            {"image": image},
+            length_penalty=length_penalty,
+            repetition_penalty=repetition_penalty,
+        )
     else:
         caption = model.generate(
-            {"image": image}, use_nucleus_sampling=True, num_captions=3, repetition_penalty=repetition_penalty, temperature=temperature
+            {"image": image},
+            use_nucleus_sampling=True,
+            num_captions=3,
+            repetition_penalty=repetition_penalty,
+            temperature=temperature,
         )
 
     caption = "\n".join(caption)
@@ -93,34 +108,73 @@ def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as blip2:
         with gr.Column():
             model_type = gr.Dropdown(
-                model_typelist, label="model list", interactive=True, type="index")
+                model_typelist, label="model list", interactive=True, type="index"
+            )
             with gr.Row():
                 with gr.Column():
-                    caption_type = gr.Radio(choices=["Beam Search", "Nucleus Sampling"], label='Type', value="Beam Search", interactive=True)
+                    caption_type = gr.Radio(
+                        choices=["Beam Search", "Nucleus Sampling"],
+                        label="Type",
+                        value="Beam Search",
+                        interactive=True,
+                    )
                     with gr.Tab("Beam Search"):
                         length_penalty = gr.Slider(
-                            label="Length Penalty", minimum=-1, maximum=2, value=1, interactive=True)
+                            label="Length Penalty",
+                            minimum=-1,
+                            maximum=2,
+                            value=1,
+                            interactive=True,
+                        )
                         repetition_penalty = gr.Slider(
-                            label="Repeat Penalty", minimum=1, maximum=5, value=1.5, interactive=True)
+                            label="Repeat Penalty",
+                            minimum=1,
+                            maximum=5,
+                            value=1.5,
+                            interactive=True,
+                        )
                     with gr.Tab("Nucleus Sampling"):
                         temperature = gr.Slider(
-                            label="Temperature", minimum=0.5, maximum=1, value=1, interactive=True)
+                            label="Temperature",
+                            minimum=0.5,
+                            maximum=1,
+                            value=1,
+                            interactive=True,
+                        )
                         repetition_penalty = gr.Slider(
-                            label="Repeat Penalty", minimum=1, maximum=5, value=1.5, interactive=True)
+                            label="Repeat Penalty",
+                            minimum=1,
+                            maximum=5,
+                            value=1.5,
+                            interactive=True,
+                        )
                     btn_caption = gr.Button("Generate Caption", variant="primary")
                 input_image = gr.Image(label="Image", type="pil")
             output_text = gr.Textbox(label="Answer", lines=5, interactive=False)
             with gr.Row():
-                send_to_buttons = modules.generation_parameters_copypaste.create_buttons(["txt2img", "img2img", "extras"])
+                send_to_buttons = (
+                    modules.generation_parameters_copypaste.create_buttons(
+                        ["txt2img", "img2img", "extras"]
+                    )
+                )
 
-        modules.generation_parameters_copypaste.bind_buttons(send_to_buttons, "", output_text)
+        modules.generation_parameters_copypaste.bind_buttons(
+            send_to_buttons, "", output_text
+        )
 
         model_type.change(load_model, inputs=[model_type])
 
         btn_caption.click(
-            gen_caption, inputs=[input_image, caption_type, length_penalty, repetition_penalty, temperature], outputs=[output_text]
+            gen_caption,
+            inputs=[
+                input_image,
+                caption_type,
+                length_penalty,
+                repetition_penalty,
+                temperature,
+            ],
+            outputs=[output_text],
         )
-
 
         return [(blip2, "BLIP2", "blip2")]
 
